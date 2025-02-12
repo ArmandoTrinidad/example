b6e4b12301c93d054f642d5dd33d041aa1a16254
ArmandoTrinidad <71052121+ArmandoTrinidad@users.noreply.github.com>
2025-02-12 12:52:51 -0600

    PRUEBA 5

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,9 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-Route::get('/HOLA MUNDO', function () {
-    return view('HOLA MUNDO'); 
+
+Route::get('/HOLA_MUNDO', function () {
+    return view('HOLA_MUNDO'); 
 });
 
 require __DIR__.'/auth.php';
